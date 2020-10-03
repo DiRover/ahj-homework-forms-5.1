3f3d80c362982dd95c95b9ec60f7582748bc93d4
Dmitriy Rovenskiy <arhitec@bk.ru>
2020-10-03 10:57:23 +0300

    code is done

---
+++ e2e/e2e.test.js
@@ -0,0 +1,43 @@
+import puppetteer from 'puppeteer';
+import { fork } from 'child_process';
+
+jest.setTimeout(60000); // default puppeteer timeout
+
+describe('Credit Card Validator form', () => {
+  let browser = null;
+  let page = null;
+  let server = null;
+  const baseUrl = 'http://localhost:9000';
+
+  beforeAll(async () => {
+    server = fork(`${__dirname}/e2e.server.js`);
+    await new Promise((resolve, reject) => {
+      server.on('error', reject);
+      server.on('message', (message) => {
+        if (message === 'ok') {
+          resolve();
+        }
+      });
+    });
+
+    browser = await puppetteer.launch({
+      // headless: false, // show gui
+      // slowMo: 250,
+      // devtools: true, // show devTools
+    });
+    page = await browser.newPage();
+  });
+
+  afterAll(async () => {
+    await browser.close();
+    server.kill();
+  });
+
+  test('check popover', async () => {
+    await page.goto(baseUrl);
+    //const popover = await page.$('[data-widget=popover-widget]');
+    const submit = await form.$('[data-btn=btn-submit]');
+    submit.click();
+    await page.waitForSelector('[data-widget=popover-widget].active');
+  });
+})
---
+++ src/js/app.js
@@ -0,0 +1,8 @@
+const btn = document.querySelector('.btn');
+const popover = document.querySelector('.popover');
+
+btn.addEventListener('click', (e) => {
+    e.preventDefault();
+    console.log('ok')
+    popover.classList.toggle('active');
+})
